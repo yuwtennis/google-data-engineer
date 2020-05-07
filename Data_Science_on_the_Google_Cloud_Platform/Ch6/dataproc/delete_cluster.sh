4c45978336c87d3b7477694d882ce8646e896b08
ywatanabe <yu.w.tennis@gmail.com>
2020-05-07 09:38:23 +0900

    Fixed typo

--- Data_Science_on_the_Google_Cloud_Platform/Ch6/dataproc/delete_cluster.sh
+++ Data_Science_on_the_Google_Cloud_Platform/Ch6/dataproc/delete_cluster.sh
@@ -8,5 +8,4 @@ if [[ $ret == 0 ]]; then
   gcloud dataproc clusters delete ${CLUSTER_NAME} --region ${REGION} --quiet
 else
   echo "There is not running cluster name ${CLUSTER_NAME} in region ${REGION}"
-"
 fi
