68741358085d264599f025acc5a7ece75b26a6e9
ywatanabe <yu.w.tennis@gmail.com>
2024-10-21 20:31:37 +0900

    chore: Wrapper for running training job

--- Ch09/create_job.sh
+++ Ch09/create_job.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-TRAIN_DATA=/gcs/elite-caster-125113-jp/flights/train.csv
-EVAL_DATA=/gcs/elite-caster-125113-jp/flights/test.csv
-OUTPUT_DIR=/gcs/elite-caster-125113-jp/flights/output/
+REGION=$(gcloud config get compute/region)
+PROJECT_ID=$(gcloud config get core/project)
+BUCKET_NAME=${PROJECT_ID}-jp
+TRAIN_DATA=/gcs/${BUCKET_NAME}/flights/train.csv
+EVAL_DATA=/gcs/${BUCKET_NAME}/flights/test.csv
+OUTPUT_DIR=/gcs/${BUCKET_NAME}/flights/output/
+CONTAINER_IMAGE_URI=${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPOS_NAME:-custom-training}/flights:${TAG:-$(git rev-parse HEAD)}
+MACHINE_TYPE=n2-standard-4
 
 gcloud ai custom-jobs create \
   --region=asia-northeast1 \
   --display-name=flights \
   --config=config.yaml \
   --args=--traindata="$TRAIN_DATA",--evaldata="$EVAL_DATA",--output="$OUTPUT_DIR" \
-  --worker-pool-spec=container-image-uri="$CONTAINER_IMAGE_URI"
+  --worker-pool-spec=replica-count=1,machine-type=${MACHINE_TYPE},container-image-uri="$CONTAINER_IMAGE_URI"
 
